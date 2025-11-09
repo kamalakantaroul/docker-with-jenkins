7dffe153b103ecfa5fd38093509f8891608650ed
kamalakantaroul <kamalakantaroul14@gmail.com>
2025-11-09 13:01:11 +0530

    first

---
+++ src/main/java/com/example/docker_with_jenkins/DockerWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_with_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerWithJenkinsApplication.class, args);
+	}
+
+}
